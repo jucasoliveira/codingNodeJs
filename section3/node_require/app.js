6164a8945cb54cf8e92d18da2f61cfbf42a03f7b
LuDeOliv <ludeoliv@ie.ibm.com>
2017-06-15 16:26:50 +0100

    Fix #4

--- section3/node_require/app.js
+++ section3/node_require/app.js
@@ -3,6 +3,7 @@ console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes');
+const _ = require('lodash');
 
 var res = notes.addNote();
 console.log(res);
@@ -10,6 +11,17 @@ console.log(res);
 //Chalenge create a export function add wich will recieve a and b,
 //sum this variables and print the result(look a notes.js too)
 console.log("Result : ", notes.add(2,23));
+
+//Finish
+
+//console.log(_.isString(true));
+//console.log(_.isString('Andrew'));
+
+var filteredArray = _.uniq(['Lucas', 1 , 'Lucas',1,2,3,4])
+console.log(filteredArray);
+
+
+
 
 /*
 var user = os.userInfo();
