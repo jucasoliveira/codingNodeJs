0ea3abfb527dbc464db7299f8717f36411d7ba16
LuDeOliv <ludeoliv@ie.ibm.com>
2017-06-15 16:09:15 +0100

    Fix #2

--- section3/node_require/app.js
+++ section3/node_require/app.js
@@ -1,8 +1,18 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
 
+var res = notes.addNote();
+console.log(res);
+
+//Chalenge create a export function add wich will recieve a and b,
+//sum this variables and print the result(look a notes.js too)
+console.log("Result : ", notes.add(2,23));
+
+/*
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`);
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
+*/
