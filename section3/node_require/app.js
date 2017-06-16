ffae8191181dbfb7b6778ba27e2f73b71fc333a8
LuDeOliv <ludeoliv@ie.ibm.com>
2017-06-16 10:08:02 +0100

    Fix #4

--- section3/node_require/app.js
+++ section3/node_require/app.js
@@ -1,30 +1,37 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes');
 const _ = require('lodash');
+const yargs = require('yargs');
 
+const notes = require('./notes');
+
+/*
 var res = notes.addNote();
 console.log(res);
-
 //Chalenge create a export function add wich will recieve a and b,
 //sum this variables and print the result(look a notes.js too)
 console.log("Result : ", notes.add(2,23));
 
 //Finish
 
-//console.log(_.isString(true));
-//console.log(_.isString('Andrew'));
-
-var filteredArray = _.uniq(['Lucas', 1 , 'Lucas',1,2,3,4])
+var filteredArray = _.uniq(['Mike', 1 , 'Lucas',1,2,3,4])
 console.log(filteredArray);
-
-
-
-
-/*
-var user = os.userInfo();
-
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
 */
+var argv = yargs.argv;
+var command = process.argv[2];
+
+console.log('Command: ', command);
+console.log('Yargs', argv);
+
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body)
+} else if (command === 'list') {
+  notes.getAll()
+} else if (command === 'read') {
+  notes.getNote(argv.title)
+} else if (command === 'remove') {
+  notes.removeNote(argv.title)
+} else{
+  console.log("Command not recognized")
+}
